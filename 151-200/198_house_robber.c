8ba02b9a56b19ad06463f68be089a0dd41af754b
zelr0x <38295414+zelr0x@users.noreply.github.com>
2024-01-21 21:47:26 +0600

    Create 198_house_robber.c

---
+++ 151-200/198_house_robber.c
@@ -0,0 +1,19 @@
+/**
+ * Constraints:
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 400
+ */
+
+#define MAX(a, b) ((a) >= (b) ? (a) : (b))
+
+int rob(int* nums, int n) {
+    int prev_max = 0;
+    int curr_max = 0;
+    for (int i = 0; i < n; i++) {
+        int num = nums[i];
+        int t = curr_max;
+        curr_max = MAX(prev_max + num, curr_max);
+        prev_max = t;
+    }
+    return curr_max;
+}
