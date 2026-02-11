43e4e0e71f441c0275896bb705c32d98af539108
zelr0x <38295414+zelr0x@users.noreply.github.com>
2026-02-11 16:30:01 +0500

    Create 525_contiguous_array.go

---
+++ 501-550/525_contiguous_array.go
@@ -0,0 +1,21 @@
+func findMaxLength(nums []int) int {
+    if len(nums) < 2 {
+        return 0
+    }
+    sums := make(map[int]int)
+    sums[0] = -1
+    sum, maxLength := 0, 0
+    for i, v := range nums {
+        if v == 0 {
+            sum--
+        } else {
+            sum++
+        }
+        if prevBalanceIdx, ok := sums[sum]; ok {
+            maxLength = max(maxLength, i - prevBalanceIdx)
+        } else {
+            sums[sum] = i
+        }
+    }
+    return maxLength
+}
